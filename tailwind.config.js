d08cbc64f135b766bebddc25b6030d0a2a1c6263
Irfan-Taufiqurrahman <irfan.rachman72@gmail.com>
2022-07-31 20:49:05 +0700

    Koi Nusantara

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./public/**/*.{html,js,css}'],
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#14b8a6',
+                secondary: '#64748b',
+                dark: '#0f172a',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+            backgroundImage: {
+                'line-img': "url('/dist/img/tes-garis.png')",
+                'garis-img': "url('/dist/img/garis1.png')",
+            },
+        },
+    },
+    plugins: [],
+}
